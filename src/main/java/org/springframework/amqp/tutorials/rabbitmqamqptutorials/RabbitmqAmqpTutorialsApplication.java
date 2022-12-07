424a8179949414a8c48cde470ae090895274e345
sosoKim9966 <sosokim9966@gmail.com>
2022-12-07 11:22:05 +0900

    2022.12.07_MQ1

---
+++ src/main/java/org/springframework/amqp/tutorials/rabbitmqamqptutorials/RabbitmqAmqpTutorialsApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.amqp.tutorials.rabbitmqamqptutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAmqpTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqAmqpTutorialsApplication.class, args);
+    }
+
+}
